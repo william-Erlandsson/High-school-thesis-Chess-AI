b5e7226d1c3d90c36ca59e4f0b4fe35f76ac57b0
william-Erlandsson <114669520+william-Erlandsson@users.noreply.github.com>
2024-01-25 12:01:01 +0100

    Changed the orders of some objects  to more closely resemble the order of data in FEN type notation.

--- Chess_AI.py
+++ Chess_AI.py
@@ -8,6 +8,7 @@ import chess
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+import os
 
 
 # Lists and Arrays
@@ -29,21 +30,25 @@ b_bishop =   np.asarray(board.pieces(chess.BISHOP, chess.BLACK).tolist())
 b_queen  =   np.asarray(board.pieces(chess.QUEEN, chess.BLACK).tolist())
 b_king   =   np.asarray(board.pieces(chess.KING, chess.BLACK).tolist())
 
-# Bitboard for en passant squares
-ep_square =  np.zeros(64)
-
-# List of booleans for the rooks ability to castle
-castle = [
-    bool(board.castling_rights & chess.BB_A1), 
-    bool(board.castling_rights & chess.BB_H1), 
-    bool(board.castling_rights & chess.BB_A8), 
-    bool(board.castling_rights & chess.BB_H8)
-         ]
 
 # Single indice list for determining the player to move
 turn   = [board.turn]
 
-# Adds a 1 to the ep_square array to show possible en passants
+
+# List of booleans for the rooks ability to castle
+castle = [
+    bool(board.castling_rights & chess.BB_H1), 
+    bool(board.castling_rights & chess.BB_A1), 
+    bool(board.castling_rights & chess.BB_H8), 
+    bool(board.castling_rights & chess.BB_A8)
+         ]
+
+
+# Bitboard for en passant squares
+ep_square =  np.zeros(64)
+
+
+# Adds a 1 to the ep_square array to show possible en passants locations
 if isinstance(board.ep_square, int):
     np.put(ep_square, board.ep_square, 1)
 
@@ -54,5 +59,5 @@ bitboard = np.concatenate((w_pawn.astype(int),   w_rook.astype(int),  w_knight.a
                            w_bishop.astype(int), w_queen.astype(int), w_king.astype(int), 
                            b_pawn.astype(int),   b_rook.astype(int),  b_knight.astype(int), 
                            b_bishop.astype(int), b_queen.astype(int), b_king.astype(int),
-                           ep_square,            castle,              turn
+                           turn,                 castle,              ep_square
                          ))
